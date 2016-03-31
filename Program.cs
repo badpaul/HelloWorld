7ac98f066981e040f4219ff38de4b8fb51e39afc
Paul Miles <paul@paulmiles.net>
2016-03-30 21:34:53 -0400

    Initial commit of Hello World!

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("You entered the following {0} command line arguments:",
+               args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("{0}", args[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
